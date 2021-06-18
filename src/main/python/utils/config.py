c33694f95f14869eb7047d58b564b7cd57321c2a
Daren Liang <daren@darenliang.com>
2021-06-18 10:08:10 -0400

    Fix dscli version

--- src/main/python/utils/config.py
+++ src/main/python/utils/config.py
@@ -1,7 +1,7 @@
 import platform
 from pathlib import Path
 
-dsgui_version = "1.3.1"
-dscli_version = "2.0.0"
+dsgui_version = "1.4.0"
+dscli_version = "1.9.0"
 
 dscli_bin = f"{Path.home().as_posix()}/.dsgui/bin/dscli{'.exe' if platform.system() == 'Windows' else ''}"
